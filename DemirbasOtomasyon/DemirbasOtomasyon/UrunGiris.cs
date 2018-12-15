32fef54317a0f5ccdf5b4a92c212a6a139b07e48
Özgür ÇAKICI <ozgurc.09@hotmail.com>
2018-12-15 17:30:53 +0300

    add

--- DemirbasOtomasyon/DemirbasOtomasyon/UrunGiris.cs
+++ DemirbasOtomasyon/DemirbasOtomasyon/UrunGiris.cs
@@ -11,6 +11,7 @@ using DevExpress.XtraEditors;
 
 namespace DemirbasOtomasyon
 {
+    
     public partial class UrunGiris : DevExpress.XtraEditors.XtraForm
     {
         public UrunGiris()
@@ -20,7 +21,18 @@ namespace DemirbasOtomasyon
 
         private void UrunGiris_Load(object sender, EventArgs e)
         {
+            DatabaseContext db = new DatabaseContext();
 
+            Urunler u = new Urunler();
+            u.urunAd = "LAPTOP";
+            u.fiyat = 50;
+            u.urunID = 1;
+            u.stokMiktari = 10;
+            u.stokDurum = false;
+            u.stokAltLimit = 1;
+            u.satinAlinmaTarihi = DateTime.Parse("2018-09-01");
+            db.Urunler.Add(u);
+            db.SaveChanges();
         }
     }
 }
